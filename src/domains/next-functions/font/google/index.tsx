fb9741bc5b8cb166dbdd1cbdeda2a658f4e32e48
TK-moon <tkmoon.dev@gmail.com>
2024-07-12 18:04:07 +0900

    Font

--- src/domains/next-functions/font/google/index.tsx
+++ src/domains/next-functions/font/google/index.tsx
@@ -1,7 +1,38 @@
+import { baskervville, noto_sans } from "@/styles/fonts";
+import Head from "next/head";
 import { FC } from "react";
+import styles from "./index.module.css";
 
 const GoogleMain: FC = () => {
-  return <main>Google</main>;
+  return (
+    <main>
+      {/* <p style={{ fontSize: 50 }}>FONT : DEFAULT</p>
+      <p>CLS TEST</p>
+      <hr />
+      <p className={noto_sans.className} style={{ fontSize: 50 }}>
+        FONT : NOTO SANS
+      </p>
+      <p>CLS TEST</p>
+      <hr /> */}
+      <p className={baskervville.className} style={{ fontSize: 50 }}>
+        FONT : NEXT
+      </p>
+      <p>CLS TEST</p>
+      <hr />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Baskervville&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <p className={styles.baskervville} style={{ fontSize: 50 }}>
+        FONT : REMOTE
+      </p>
+      <p>CLS TEST</p>
+    </main>
+  );
 };
 
 export { GoogleMain };
