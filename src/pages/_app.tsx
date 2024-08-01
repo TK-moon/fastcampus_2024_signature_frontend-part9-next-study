363e7b3556245bf4b016641d37360aa8ca536bc9
TK-moon <tkmoon.dev@gmail.com>
2024-08-01 18:11:08 +0900

    useRouter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+let href = "";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      console.log("ROUTE CHANGE START");
+      href = window.location.href;
+    });
+    router.events.on("routeChangeComplete", () => {
+      console.log("ROUTE CHANGE END", "from=", href);
+      href = "";
+    });
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
